be5c7b36a3e98321a4ca6912017423f0ef1d9225
劉哲宇 <j2325138@gmail.com>
2018-12-08 18:52:01 +0800

    Add a 'at least' argument for clean_empty.py'

--- clean_empty.py
+++ clean_empty.py
@@ -1,14 +1,19 @@
 import shutil
 import os
 from glob import glob
+import argparse
 
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--at-least', type=int, default=1)
+args = parser.parse_args()
 
 for direc in glob('saved/*/*'):
     splits = direc.split('/')
     if splits[1] == 'runs':
         continue
     hey = glob(os.path.join(direc, '*.pth'))
-    if len(hey) == 0:
+    if len(hey) < args.at_least:
         splits.insert(1, 'runs')
         runs_dir = '/'.join(splits)
         print(direc)
