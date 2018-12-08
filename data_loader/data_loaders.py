aebfc161d64abefa67df173e34728c06aefce851
劉哲宇 <j2325138@gmail.com>
2018-12-08 23:13:52 +0800

    Add SampleDataloader; Add configurable dataset_args

--- data_loader/data_loaders.py
+++ data_loader/data_loaders.py
@@ -1,5 +1,29 @@
 from torchvision import datasets, transforms
+from torch.utils.data.dataset import Dataset
+
 from base import BaseDataLoader
+
+
+class SampleDataLoader(BaseDataLoader):
+    def __init__(
+        self, data_dir, batch_size,
+        shuffle, validation_split,
+        num_workers, dataset_args={}
+    ):
+        self.data_dir = data_dir
+        self.dataset = SampleDataSet(data_dir, **dataset_args)
+        super(SampleDataLoader, self).__init__(self.dataset, batch_size, shuffle, validation_split, num_workers)
+
+
+class SampleDataSet(Dataset):
+    def __init__(self, data_dir, training):
+        self.dataset = datasets.MNIST(data_dir, train=training, download=True, transform=transforms.ToTensor())
+
+    def __getitem__(self, index):
+        return self.dataset.__getitem__(index)
+
+    def __len__(self):
+        return len(self.dataset)
 
 
 class MnistDataLoader(BaseDataLoader):
