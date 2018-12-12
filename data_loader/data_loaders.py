e4ce3f49a130028e76324e579c5280fbcec48b3f
劉哲宇 <j2325138@gmail.com>
2018-12-12 23:31:41 +0800

    Delete useless dataset; Move data_dir to dataset_args

--- data_loader/data_loaders.py
+++ data_loader/data_loaders.py
@@ -6,36 +6,28 @@ from base import BaseDataLoader
 
 class SampleDataLoader(BaseDataLoader):
     def __init__(
-        self, data_dir, batch_size,
+        self, batch_size,
         shuffle, validation_split,
         num_workers, dataset_args={}
     ):
-        self.data_dir = data_dir
-        self.dataset = SampleDataSet(data_dir, **dataset_args)
-        super(SampleDataLoader, self).__init__(self.dataset, batch_size, shuffle, validation_split, num_workers)
+        self.dataset = SampleDataSet(**dataset_args)
+        super(SampleDataLoader, self).__init__(
+            self.dataset, batch_size, shuffle,
+            validation_split, num_workers)
 
 
 class SampleDataSet(Dataset):
     def __init__(self, data_dir, training):
-        self.dataset = datasets.MNIST(data_dir, train=training, download=True, transform=transforms.ToTensor())
+        trsfm = transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize((0.1307,), (0.3081,))
+        ])
+        self.dataset = datasets.MNIST(
+            data_dir, train=training, download=True, transform=trsfm
+        )
 
     def __getitem__(self, index):
         return self.dataset.__getitem__(index)
 
     def __len__(self):
         return len(self.dataset)
-
-
-class MnistDataLoader(BaseDataLoader):
-    """
-    MNIST data loading demo using BaseDataLoader
-    """
-
-    def __init__(self, data_dir, batch_size, shuffle, validation_split, num_workers, training=True):
-        trsfm = transforms.Compose([
-            transforms.ToTensor(),
-            transforms.Normalize((0.1307,), (0.3081,))
-        ])
-        self.data_dir = data_dir
-        self.dataset = datasets.MNIST(self.data_dir, train=training, download=True, transform=trsfm)
-        super(MnistDataLoader, self).__init__(self.dataset, batch_size, shuffle, validation_split, num_workers)
