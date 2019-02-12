5bd04314471575616690b184c85aeaffe5afabb2
劉哲宇 <j2325138@gmail.com>
2019-02-12 17:55:24 +0800

    colored logger

--- utils/logger.py
+++ utils/logger.py
@@ -1,11 +1,25 @@
 import json
+import sys
 import logging
 
 
+stream_handler = logging.StreamHandler(sys.stdout)
+format_ = ('[%(asctime)s] {%(filename)s:%(lineno)d} '
+           '%(levelname)s - %(message)s')
+
+try:
+    # use colored logs if installed
+    import coloredlogs
+    formatter = coloredlogs.ColoredFormatter(fmt=format_)
+    stream_handler.setFormatter(formatter)
+except Exception:
+    pass
+
 logging.basicConfig(
     level=logging.INFO,
-    format='[%(asctime)s] %(message)s',
+    format=format_,
     datefmt='%m-%d %H:%M:%S',
+    handlers=[stream_handler]
 )
 
 
